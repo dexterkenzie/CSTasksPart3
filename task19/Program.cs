ca41382b972cc231cd8cc9712068d1ada5dc7213
«dashkovsky» <>
2022-09-10 19:44:26 +0300

    task 19 done

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,7 +1,38 @@
-﻿// Задача 19
+﻿using System;
+
+// Задача 19
 // Напишите программу, которая принимает на вход пятизначное число и 
 // проверяет, является ли оно палиндромом.
 
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+string fullNumber = Console.ReadLine();
+
+char[] ch = new char[fullNumber.Length];
+
+for (int i = 0; i < fullNumber.Length; i++)
+{
+    ch[i] = fullNumber[i];
+}
+
+foreach (char c in ch)
+{
+    Console.WriteLine(c);
+}
+
+if (fullNumber.Length == 5)
+{
+    int num1 = fullNumber[0];
+    int num2 = fullNumber[1];
+    int num4 = fullNumber[3];
+    int num5 = fullNumber[4];
+    if (num1 == num5 && num2 == num4)
+        Console.WriteLine("Число является палиндромом.");
+        else
+            Console.WriteLine("Число не является палиндромом.");
+}
+else
+    Console.WriteLine("Вы ввели некорректное число. Нужно набрать пятизначное число. Пожалуйста, попробуйте снова.");
